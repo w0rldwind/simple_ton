6598ad46b4de2469b1541f296394748b4a0fa8f7
w0rldwind <65605792+w0rldwind@users.noreply.github.com>
2020-12-14 17:05:22 +0300

    Update dpinfo.sh

--- Monitoring/dpinfo.sh
+++ Monitoring/dpinfo.sh
@@ -5,14 +5,16 @@ depool=
 
 dpinfo=$(./tonos-cli run $depool getParticipantInfo '{"addr":"'${msig}'"}' --abi DePool.abi.json)
 
-stake1=$(echo "$dpinfo" | grep -oP '(?<="0x5": ")[^"]*')
-stake1=$(printf '%x' ${stake1})
+stake=$(echo "$dpinfo" | grep -oP '(?<="0x[0-9]": ")[^"]*')
+
+staken=($stake)
+
+stake1=$(printf '%x' ${staken[0]})
 stake1=$((16#${stake1}))
 stake1=$(bc -l <<< "scale=2; ${stake1}/1000000000")
 echo Stake 1: $stake1
 
-stake2=$(echo "$dpinfo" | grep -oP '(?<="0x6": ")[^"]*')
-stake2=$(printf '%x' ${stake2})
+stake2=$(printf '%x' ${staken[1]})
 stake2=$((16#${stake2}))
 stake2=$(bc -l <<< "scale=2; ${stake2}/1000000000")
 echo Stake 2: $stake2
